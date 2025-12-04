34048c50fb9abb824467c112c7254d51e1cdc9d0
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-12-04 14:56:42 -0600

    Callbacks.

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,9 +1,19 @@
-const { emailTemplate } = require('./js-foundation/01-template.js');
-require('./js-foundation/02-desestructuring.js')
+// const { emailTemplate } = require('./js-foundation/01-template.js');
+// require('./js-foundation/02-desestructuring.js')
+const {getUserById} = require ('./js-foundation/03-callbacks.js');
+
 
 // console.log(emailTemplate);
-
-
 /* module.exports = {
     emailTemplate
 }; */
+
+const id = 1;
+
+getUserById(id, function(error, user){
+    if(error){
+        throw new Error(error);
+    };
+
+    console.log(user);
+});
