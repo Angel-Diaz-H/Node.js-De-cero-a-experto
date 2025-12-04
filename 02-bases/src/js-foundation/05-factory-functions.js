0f9af5463ee9e866118b9e8c7b255049992d2106
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-12-04 15:34:11 -0600

    Factory functions - Necesidad

--- 02-bases/src/js-foundation/05-factory-functions.js
+++ 02-bases/src/js-foundation/05-factory-functions.js
@@ -1,10 +1,12 @@
+const {v4: uuidv4} = require('uuid');
+const getAge = require('get-age');
 
 const builPerson = ({ name, birthday }) => {
     return {
-        id: new Date().getTime(),
+        id: uuidv4(),
         name,
         birthday,
-        age: new Date().getFullYear() - new Date(birthday).getFullYear(),
+        age: getAge(birthday),
     };
 };
 
