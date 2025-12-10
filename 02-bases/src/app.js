ae7fbab8803e550e62c64491c1b49b6719d8fbcc
Angel-Diaz-H <angeldiaz.hrn@gmail.com>
2025-12-10 14:48:53 -0600

    Promesas

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,24 +1,4 @@
-// const { emailTemplate } = require('./js-foundation/01-template.js');
-// require('./js-foundation/02-desestructuring.js')
-// const { getUserById } = require('./js-foundation/05-factory-functions.js');
-const { builPerson } = require('./js-foundation/05-factory-functions.js');
 const { getAge, getUUID } = require('./plugins');
+const getPokemonById = require('./js-foundation/06-promises.js');
 
-// console.log(emailTemplate);
-/* module.exports = {
-    emailTemplate
-}; */
-
-/* const id = 1;
-
-getUserById(id, (error, user) => {
-    if (error) throw new Error(error);
-
-    console.log(user);
-}); */
-
-const makePerson = builPerson({ getUUID, getAge })
-const obj = { name: 'Angel', birthdate: '2000-01-01' };
-
-const john = makePerson(obj);
-console.log(john);
+console.log(getPokemonById(1));
