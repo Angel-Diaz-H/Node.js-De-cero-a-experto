e053842b059d46ae8c7d7058e5de5680ebfa1831
Angel Díaz <angeldiaz.hrn@gmail.com>
2025-12-30 06:31:11 -0600

    Async - Await

--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,10 +1,14 @@
-const getPokemonById = (id) => {
+const getPokemonById = async(id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const resp = await fetch(url);
+    const pokemon = await resp.json();
+    return pokemon.name;
+    
+    // return fetch(url)
+    //     .then((resp) => resp.json())
+    //     .then(() => { throw new Error('Pokemon no salio mal') })
+    //     .then((pokemon) => pokemon.name);
 
-    return fetch(url)
-        .then((resp) => resp.json())
-        .then(() => { throw new Error('Pokemon no salio mal') })
-        .then((pokemon) => pokemon.name);
 };
 
 module.exports = getPokemonById;
